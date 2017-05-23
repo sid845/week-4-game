38ca97714147246c332ee56bc3f807a4ae965ff1
unknown <siddharthpatel845@gmail.com>
2017-05-22 21:02:30 -0400

    initial commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,53 @@
 $(document).ready(function() 
 {
-	var targetNumber=[33,23,67];
+	var targetNumber=[33,23,67,53];
 	var number0=[10,20,2,1];
 	var number1=[5,5,3,7];
 	var number2=[20,30,7,10];
+	var number3=[20,10,5,3];
 	var rtnumber=targetNumber[Math.floor(Math.random()*targetNumber.length)];
-	console.log(rtnumber);
 	console.log(rtnumber);
 	var index=targetNumber.indexOf(rtnumber);
 	console.log(index);
-    console.log( "ready!" );
-
-	for(i=0;i<3;i++)
-	{
-		if(index==i)
+		if(index===0)
 		{ 
 			console.log(index);			
-			document.getElementById("#sysScore").innerHTML += index;
+			$("#sysScore").append(index);
+			for(i=0;i<4;i++)
+			{
+				$("stone").attr("data-crystalvalue", number0[i]);
+			}
 
 		}
-	}
+		if(index===1)
+		{ 
+			console.log(index);			
+			$("#sysScore").append(index);
+			for(i=0;i<4;i++)
+			{
+				$("stone").attr("data-crystalvalue", number1[i]);
+			}
+
+		}
+		if(index===2)
+		{ 
+			console.log(index);			
+			$("#sysScore").append(index);
+			for(i=0;i<4;i++)
+			{
+				$("stone").attr("data-crystalvalue", number2[i]);
+			}
+
+		}
+		if(index===3)
+		{ 
+			console.log(index);			
+			$("#sysScore").append(index);
+			for(i=0;i<4;i++)
+			{
+				$("stone").attr("data-crystalvalue", number3[i]);
+			}
+
+		}
+	
 });
